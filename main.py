81adf6f75b32072c19c8b1026cf1ce74c2a851d9
Marcos F3rnando Morales <125113310+Marcos-F-Morales@users.noreply.github.com>
2025-03-21 15:53:24 -0600

    cambios
    se cambiaron funciones en los errores para que busque por fila y posicion

--- main.py
+++ main.py
@@ -2,35 +2,35 @@ from src.utils.files import Files as f
 from src.lexer.lexer import Lexer
 from src.sintactic.parser import Parser
 from src.utils.errors import Errors
+
 class Main:
     def __init__(self):
         """
         Inicializa el compilador.
         """
-        self.lexic_errors=Errors() #Errores lexicos -> Graficarlos html
-        self.sintatic_errors=Errors() #Errores sintacticos -> Graficarlos html
+        self.lexic_errors = Errors()  # Errores léxicos -> Graficarlos HTML
+        self.sintatic_errors = Errors()  # Errores sintácticos -> Graficarlos HTML
 
         file = f("lenguaje.txt")
         content, lines = file.read_file()
 
         if content:
             try:
-                self.lexer = Lexer(self.lexic_errors) 
-                self.parser = Parser(self.lexer,self.sintatic_errors)  
+                self.lexer = Lexer(self.lexic_errors)
+                self.parser = Parser(self.lexer, self.sintatic_errors)  
                 
                 tokens = self.lexer.tokenize(content)
                 print("Tokens encontrados:", tokens)
 
                 # Iniciar análisis sintáctico
                 self.parser.parse(content)
-                self.sintatic_errors.errorHtml('Sintacticos')
-                self.lexic_errors.errorHtml('Lexicos')
+                self.sintatic_errors.errorHtml('Sintácticos')
+                self.lexic_errors.errorHtml('Léxicos')
 
             except Exception as e:
                 print("Error: ", e)
         else:
             print("No se pudo leer el archivo.")
-
 
 if __name__ == "__main__":
     Main()
